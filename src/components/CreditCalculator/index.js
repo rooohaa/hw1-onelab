96bb8cbabf70abb5fd8c55acea5ad140802f2420
Rakhat Alpysbai <rekebek02@gmail.com>
2021-10-25 23:42:30 +0600

    credit calculator in progress

--- src/components/CreditCalculator/index.js
+++ src/components/CreditCalculator/index.js
@@ -1,10 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Slider, Switch } from '@mui/material'
+import { AppButton } from 'sc/AppButton'
 import { CalculatorWrapper } from './style'
 
+const MIN_CREDIT_VALUE = 10000
+const MIN_CREDIT_TERM = 6
+
 const CreditCalculator = () => {
+  const [creditValue, setCreditValue] = useState(MIN_CREDIT_VALUE)
+  const [creditTerm, setCreditTerm] = useState(MIN_CREDIT_TERM)
+  const [discount, setDiscount] = useState(false)
+
+  const rate = discount ? 18.99 : 20.99
+
+  const calculateAnnuityPayment = () => {
+    const interestRate = rate / (100 * 12)
+
+    const factor =
+      (interestRate * (1 + interestRate) ** creditTerm) /
+        (1 + interestRate) ** creditTerm -
+      1
+
+    return creditValue * factor
+  }
+
+  const res = calculateAnnuityPayment()
+
+  console.log(res)
+
   return (
     <CalculatorWrapper>
-      <p>calc</p>
+      <div className="calc-grid">
+        <div className="slider-block">
+          <p className="calc-text">Сколько вам нужно?</p>
+
+          <div className="slider-wrapper">
+            <div className="d-flex">
+              <Slider
+                color="secondary"
+                size="small"
+                value={creditValue}
+                step={110_000}
+                onChange={(e) => setCreditValue(e.target.value)}
+                min={10000}
+                max={10_000_000}
+              />
+              <div className="slider-range">
+                <span>10 000 ₸</span>
+                <span>10 000 000 ₸</span>
+              </div>
+            </div>
+            <p className="credit-val">{creditValue.toLocaleString()} ₸</p>
+          </div>
+
+          <p className="calc-text">Срок кредитования</p>
+
+          <div className="slider-wrapper">
+            <div className="d-flex">
+              <Slider
+                color="secondary"
+                size="small"
+                value={creditTerm}
+                step={2}
+                onChange={(e) => setCreditTerm(e.target.value)}
+                min={6}
+                max={60}
+              />
+              <div className="slider-range">
+                <span>6 месяцев</span>
+                <span>5 лет</span>
+              </div>
+            </div>
+            <p className="credit-val">{creditTerm} месяцев</p>
+          </div>
+
+          <div className="switch-wrap">
+            <Switch
+              color="secondary"
+              checked={discount}
+              onChange={(e) => setDiscount(e.target.checked)}
+            />
+            <p className="calc-text">Я получаю запрплату по карте ForteBank</p>
+          </div>
+
+          <p className="message">
+            Для точного расчета необходимо оставить заявку
+          </p>
+        </div>
+
+        <div className="credit-result">
+          <div>
+            <p className="calc-text sm">Ежемесячный платеж</p>
+            <span className="month-amount">
+              {calculateAnnuityPayment().toLocaleString()} ₸
+            </span>
+          </div>
+
+          <div>
+            <p className="calc-text sm">Ставка вознаграждения</p>
+            <span className="p-rate">{rate}%</span>
+          </div>
+
+          <AppButton>Оформить кредит</AppButton>
+        </div>
+      </div>
     </CalculatorWrapper>
   )
 }
--- src/components/CreditCalculator/style.js
+++ src/components/CreditCalculator/style.js
@@ -6,6 +6,56 @@ export const CalculatorWrapper = styled.div`
   padding: 32px;
   width: 100%;
 
+  .calc-grid {
+    display: flex;
+    align-items: flex-start;
+
+    .slider-block {
+      width: 70%;
+      padding-right: 35px;
+      border-right: 1px solid #e0e0e0;
+    }
+  }
+
+  .slider-wrapper {
+    display: flex;
+    align-items: center;
+    column-gap: 16px;
+
+    margin-top: 13px;
+    margin-bottom: 24px;
+
+    .d-flex {
+      width: 100%;
+
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      row-gap: 8px;
+
+      .slider-range {
+        width: 100%;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        span {
+          font-size: 12px;
+          line-height: 18px;
+          color: #737373;
+        }
+      }
+    }
+
+    p.credit-val {
+      font-size: 21px;
+      font-weight: 400;
+      line-height: 26px;
+      color: #303030;
+    }
+  }
+
   p.calc-text {
     font-size: 16px;
     line-height: 24px;
@@ -13,7 +63,50 @@ export const CalculatorWrapper = styled.div`
 
     &.sm {
       font-size: 14px;
+      font-weight: 300;
       line-height: 21px;
+    }
+  }
+
+  .switch-wrap {
+    display: flex;
+    align-items: center;
+    column-gap: 16px;
+
+    margin-bottom: 20px;
+  }
+
+  .message {
+    color: #737373;
+    font-size: 14px;
+    line-height: 19px;
+    font-weight: 300;
+  }
+
+  .credit-result {
+    padding-left: 35px;
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    row-gap: 24px;
+
+    .calc-text.sm {
+      margin-bottom: 8px;
+    }
+
+    .month-amount {
+      color: #9d2550;
+
+      font-size: 30px;
+      font-weight: 500;
+      line-height: 36px;
+    }
+
+    .p-rate {
+      font-size: 24px;
+      line-height: 28px;
+      color: #303030;
     }
   }
 `
